659939043e1bb7cbc0ddd26ae96f7196daf99e11
exer <98556122+ekkusa@users.noreply.github.com>
2023-08-02 12:53:01 +0800

    [anykernel]: Set is_slot_device to 1

--- anykernel.sh
+++ anykernel.sh
@@ -36,7 +36,7 @@ set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
 ## boot shell variables
 block=boot;
-is_slot_device=0;
+is_slot_device=1;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
 
