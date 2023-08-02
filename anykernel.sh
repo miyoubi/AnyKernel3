11afb9431bf6a6d75c3d829af8931e5f1aa74c08
exer <98556122+ekkusa@users.noreply.github.com>
2023-08-02 12:52:33 +0800

    [anykernel]: Set block to use boot

--- anykernel.sh
+++ anykernel.sh
@@ -34,8 +34,8 @@ set_perm_recursive 0 0 755 644 $ramdisk/*;
 set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 } # end attributes
 
-# boot shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+## boot shell variables
+block=boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
