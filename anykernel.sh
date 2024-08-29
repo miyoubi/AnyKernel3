a3c2326d7463f0413b48b0cb8d3fd2c4a1f22c7b
exer <98556122+ekkusa@users.noreply.github.com>
2024-08-29 18:15:16 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=miyoubi:db-r2
+kernel.string=miyoubi:dbr2
 device.name=placeholder
 kernel.for=placeholder
 kernelsu.version=n
